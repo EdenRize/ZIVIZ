e8639b0d81008f07421170800976d89593b8f907
EdenRize <edenrize@gmail.com>
2024-02-14 12:11:01 +0200

    Add more imgs to merch

--- src/cmps/MerchSection.jsx
+++ src/cmps/MerchSection.jsx
@@ -8,6 +8,8 @@ export function MerchSection() {
             <p>Available <a href="https://www.ona.co.il/prd?collection=ziviz&fbclid=PAAaYNx4uKmHhCYjd3D6n_vkJykdNkDC9N_WAiXo34kDnepJ7bYm-kCnT9asE_aem_AaGFiPqPpNMd6aT2LKGvvC-DTLnYH1G66d4-WAxQLdkUwczFYbAByn3rUC1kgc5A6aM&page=2" target="_blank">here</a></p>
 
             <img className="ziv-2" src="/img/ziv2.jpeg" />
+            <img className="ziv-and-men3" src="/img/ziv+men3.jpeg" />
+            <img className="ziv-and-men4" src="/img/ziv+men4.jpeg" />
             <img className="men-3" src="/img/men3.jpeg" />
             <img className="ziv" src="/img/ziv.jpeg" />
             <img className="no-faces" src="/img/no-faces.jpeg" />
