12277b92e37c228608d5bf2acd07d7cd42840ff1
EdenRize <edenrize@gmail.com>
2024-02-15 16:19:38 +0200

    Add animations to hero

--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useRef, useState } from "react";
 import { Logo } from "./Logo";
 
 export function Hero() {
+    const [isVisible, setIsVisible] = useState(false);
+    const sectionRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                }
+            });
+        });
+
+        observer.observe(sectionRef.current);
+
+        // Cleanup observer
+        return () => observer.disconnect();
+    }, []);
     return (
-        <div className="section hero">
+        <div className={`section ${isVisible ? 'visible' : ''} hero`} ref={sectionRef}>
             <img className="hero-img women" src="/img/two-women-flower.png" />
             <div className="flex column align-center title-container">
                 <Logo />
